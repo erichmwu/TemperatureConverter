fe3780bfd50233d724f7ec53c21e981feaba46f9
Eric Wu <eric.ics211@gmail.com>
2015-02-02 14:11:30 -1000

    Answer to TemperatureConverter

---
+++ src/TemperatureConverter.java
@@ -0,0 +1,37 @@
+/**
+ * Created by Eric on 2/2/2015.
+ */
+public class TemperatureConverter {
+  public static void main(String[] args) {
+    double celsius, fahrenheit;
+    if (args.length != 2 || !isInt(args[1])) {
+      System.err.println("Illegal second argument");
+      System.exit(1);
+    }
+    if (!args[0].equalsIgnoreCase("-F") && !args[0].equalsIgnoreCase("-C")) {
+      System.err.println("Illegal first argument");
+      System.exit(1);
+    }
+
+    if (args[0].equals("-F")) {
+      celsius = (convertToInt(args[1]) - 32) * 5.0 / 9.0;
+      System.out.println(args[1] + " in C is " + celsius);
+    } else if (args[0].equals("-C")) {
+      fahrenheit = (convertToInt(args[1]) * 9.0/5.0) + 32;
+      System.out.println(args[1] + " in F is " + fahrenheit);
+    }
+  }
+
+  public static boolean isInt(String input) {
+    try {
+      Integer.parseInt(input);
+    } catch (NumberFormatException e) {
+      return false;
+    }
+    return true;
+  }
+
+  public static int convertToInt(String input) {
+    return Integer.parseInt(input);
+  }
+ }
